c7db31aa7206ca53ef3fc63ea16f2ef9919cc766
LordRoman <122212260+LordRoman@users.noreply.github.com>
2025-01-01 23:12:04 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+username = 'пользователь'
+title = 'заголовок'
+content = 'описание'
+status = 'статус'
+created_date = '10-11-2024'
+issue_date = '10-12-2024'
+
+print(f'Имя: {username}')
+print(f'Заголовок: {title}')
+print(f'Описание: {content}')
+print(f'Статус выполнения: {status}')
+print(f'Дата создания заметки в формате "день-месяц-год": {created_date}')
+print(f'Дата истечения заметки в формате "день-месяц-год": {issue_date}')
