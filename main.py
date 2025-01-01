18817c6d805dcab7af3d8fa5e825caa7a0bdbb5c
LordRoman <122212260+LordRoman@users.noreply.github.com>
2025-01-01 23:41:11 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Изначальные переменные
 username = 'пользователь'
 title = 'заголовок'
 content = 'описание'
@@ -5,9 +6,14 @@ status = 'статус'
 created_date = '10-11-2024'
 issue_date = '10-12-2024'
 
+# Создаем временные переменные без года
+temp_created_date = created_date[:5]  # Берем только день и месяц
+temp_issue_date = issue_date[:5]      # Берем только день и месяц
+
+# Вывод для пользователя
 print(f'Имя: {username}')
 print(f'Заголовок: {title}')
 print(f'Описание: {content}')
 print(f'Статус выполнения: {status}')
-print(f'Дата создания заметки в формате "день-месяц-год": {created_date}')
-print(f'Дата истечения заметки в формате "день-месяц-год": {issue_date}')
+print(f'Дата создания заметки (день-месяц): {temp_created_date}')
+print(f'Дата истечения заметки (день-месяц): {temp_issue_date}')
